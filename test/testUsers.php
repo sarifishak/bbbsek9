7ac8e6318f59dec9bdd26ee42ee6865a389311b9
Sarif Ishak <sarif.ishak@gmail.com>
2017-01-16 18:05:20 +0800

    The basic users model

---
+++ test/testUsers.php
@@ -0,0 +1,44 @@
+<?php
+  require_once(dirname(__FILE__).'/../model/users.php');
+?>
+<html>
+  <head>
+    <title>DB Query PHP Page</title>
+  </head>
+  <body>
+    <p>The debug page sites</p>
+    <p>Contents of table Users:</p>
+    <table border='1'>
+      <tr>
+        <td>Id</td>
+        <td>Username</td>
+        <td>Password</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Type</td>
+        <td>Status</td>
+        <td>CreatedDate</td>
+        <td>CraetedId</td>
+      </tr>
+    <?php 
+      //refer to user.php for the implementation of the class User 
+      $users = new Users();
+      $user_list = $users->selectAll();
+
+      foreach($user_list as $user) {
+        echo '<tr>';
+        echo '<td>'.$user->id.'</td>';
+        echo '<td>'.$user->username.'</td>';
+        echo '<td>'.$user->password.'</td>';
+        echo '<td>'.$user->firstname.'</td>';
+        echo '<td>'.$user->lastname.'</td>';
+        echo '<td>'.$user->userType.'</td>';
+        echo '<td>'.$user->status.'</td>';
+        echo '<td>'.$user->createdDate.'</td>';
+        echo '<td>'.$user->createdId.'</td>';
+        echo '</tr>';
+      }
+    ?>
+    </table> 
+  </body>
+</html>
