e0151890646e4b2eaadc6e448137c52f5037e7d0
Sarif Ishak <sarif.ishak@gmail.com>
2017-01-17 11:35:23 +0800

    Added contact module

---
+++ test/testContacts.php
@@ -0,0 +1,85 @@
+<?php
+  require_once(dirname(__FILE__).'/../model/contacts.php');
+?>
+<html>
+  <head>
+    <title>DB Query PHP Page</title>
+  </head>
+  <body>
+    <p>The debug page sites</p>
+
+  var $id;
+  var $mainId;
+  var $contactTypeId;
+  var $firstName;
+  var $lastName;
+  var $ic;
+  var $noRumah;
+  var $jalan;
+  var $address;
+  var $city;
+  var $state;
+  var $postcode;
+  var $mobile;
+  var $office;
+  var $home;
+  var $fax;
+  var $email;
+  var $createdDate;
+  var $createdId;
+  
+    <p>Contents of table Contacts:</p>
+    <table border='1'>
+      <tr>
+        <td>Id</td>
+        <td>MainId</td>
+        <td>ContactTypeId</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>IC</td>
+        <td>No Rumah</td>
+        <td>Jalan</td>
+        <td>Address</td>
+        <td>City</td>
+        <td>State</td>
+        <td>Postcode</td>
+        <td>Mobile</td>
+        <td>Office</td>
+        <td>Home</td>
+        <td>Fak</td>
+        <td>Email</td>
+        <td>CreatedDate</td>
+        <td>CraetedId</td>
+      </tr>
+    <?php 
+      //refer to user.php for the implementation of the class User 
+      $contacts = new Contacts();
+      $contacts_list = $contacts->selectAll();
+
+      foreach($contacts_list as $contact) {
+        echo '<tr>';
+        echo '<td>'.$contact->id.'</td>';
+        echo '<td>'.$contact->mainId.'</td>';
+        echo '<td>'.$contact->contactTypeId.'</td>';
+        echo '<td>'.$contact->firstName.'</td>';
+        echo '<td>'.$contact->lastName.'</td>';
+        echo '<td>'.$contact->ic.'</td>';
+        echo '<td>'.$contact->noRumah.'</td>';
+        echo '<td>'.$contact->jalan.'</td>';
+        echo '<td>'.$contact->address.'</td>';
+        echo '<td>'.$contact->city.'</td>';
+        echo '<td>'.$contact->state.'</td>';
+        echo '<td>'.$contact->postcode.'</td>';
+        echo '<td>'.$contact->mobile.'</td>';
+        echo '<td>'.$contact->office.'</td>';
+        echo '<td>'.$contact->home.'</td>';
+        echo '<td>'.$contact->fax.'</td>';
+        echo '<td>'.$contact->email.'</td>';
+        echo '<td>'.$contact->createdDate.'</td>';
+        echo '<td>'.$contact->createdId.'</td>';
+        echo '</tr>';
+      }
+    ?>
+    </table> 
+  </body>
+</html>
