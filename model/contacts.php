40292d5b7c25a544cff2910ccf59539ea85d927a
Sarif Ishak <sarif.ishak@gmail.com>
2017-01-18 16:36:28 +0800

    Added include statement in contacts.php

--- model/contacts.php
+++ model/contacts.php
@@ -1,5 +1,6 @@
 <?php
-  require_once(dirname(__FILE__).'/../dbmanager.php');  
+  require_once(dirname(__FILE__).'/../dbmanager.php');
+  require_once(dirname(__FILE__).'/receivedPayment.php');
 ?>
 <?php
 class Contacts{
