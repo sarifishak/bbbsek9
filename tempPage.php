972106de8afc24cd4ee3749f97a710836c402873
Sarif Ishak <sarif.ishak@gmail.com>
2017-02-27 20:50:45 +0800

    A quick solution for google cloud. Temppage. small mistake

--- tempPage.php
+++ tempPage.php
@@ -1,6 +1,6 @@
 <?php
-  require_once(dirname(__FILE__).'/../model/contacts.php');
-  require_once(dirname(__FILE__).'/../model/receivedPayment.php');
+  require_once(dirname(__FILE__).'/model/contacts.php');
+  require_once(dirname(__FILE__).'/model/receivedPayment.php');
   
   $searchtxt = $_REQUEST['searchtxt'];
   
